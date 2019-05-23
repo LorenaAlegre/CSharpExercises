6838ca23124ad89aaaaceccb0e4283cb4e59f151
LorenaAlegre <alegrelorena1@gmail.com>
2019-05-23 20:02:14 -0300

    ejercicios

--- 3.TiposDatos/DataTypeExercises.cs
+++ 3.TiposDatos/DataTypeExercises.cs
@@ -3,37 +3,36 @@ using System;
 namespace CSharpExercises{
     public class DataTypeExercises{
         public void ValueSum(){
-            Console.WriteLine("Ingrese un valor a sumar:");
-            var firstValue = Console.ReadLine();
+            // Console.WriteLine("Ingrese un valor a sumar:");
+            // var firstValue = Console.ReadLine();
 
-            Console.WriteLine("Ingrese otro valor a sumar:");
-            var secondValue = Console.ReadLine();
+            // Console.WriteLine("Ingrese otro valor a sumar:");
+            // var secondValue = Console.ReadLine();
             
-            int first = 0;
-            int second = 0;
-            bool couldParseFirst = Int32.TryParse(firstValue, out first);
+            // int first = 0;
+            // int second = 0;
+            // bool couldParseFirst = Int32.TryParse(firstValue, out first);
 
-            if(couldParseFirst){
-                bool couldParseSecond = Int32.TryParse(secondValue, out second);
-                if (couldParseSecond){
+            // if(couldParseFirst)
+            // {
+            //     bool couldParseSecond = Int32.TryParse(secondValue, out second);
 
-                    Console.WriteLine("El resultado de la suma es: {0}", first + second);
-                }
-            
-            }else
-            {
-                Console.WriteLine("El segundo valor no es numerico");
+            //     if (couldParseSecond)
+            //     {
+            //     Console.WriteLine("El resultado de la suma es: {0}", first + second);
+            //     }
+            //     else
+            //     {
+            //     Console.WriteLine("El segundo valor no es numerico");
+            //     }
+            //     else  {
 
-            }
-         else  {
+            //     Console.WriteLine("El primer parametro esta mal formateado");
+            //     }
+            // }
 
-                Console.WriteLine("El primer parametro esta mal formateado");
-         }
-                
-            }
-            }
-
-            Console.WriteLine("El resultado de la suma es: {0}", int.Parse(firstValue) + int.Parse(secondValue));
+            // Console.WriteLine("El resultado de la suma es: {0}", int.Parse(firstValue) + int.Parse(secondValue));
         }
     }
-}
+    }
+    
--- Program.cs
+++ Program.cs
@@ -6,19 +6,29 @@ namespace CSharpExercises
     {
         static void Main(string[] args)
         {
-            //1.
-            //ConsoleExercises console = new ConsoleExercises();
-            //console.AskAge();
-
-            //Pedir nombre, apellido y documento e imprimir toda la información al final.
-
-            //2.
-            //VariableExercises variable = new VariableExercises();
-            //variable.Concatenador();
+            Console.WriteLine("Escriba una opcion:");
             
-            //3.
-            DataTypeExercises dataType = new DataTypeExercises();
-            dataType.ValueSum();
+            string opcion = Console.ReadLine();
+        
+            switch (opcion)
+            {
+                case "1":
+                ConsoleExercises console = new ConsoleExercises();
+                console.AskAge();
+                //Pedir nombre, apellido y documento e imprimir toda la información al final.
+                break;
+
+                case "2":
+                VariableExercises variable = new VariableExercises();
+                variable.Concatenador();
+                break;
+
+                case "3":
+                DataTypeExercises dataType = new DataTypeExercises();
+                dataType.ValueSum();
+                break;
+            }
+        
         }
     }
 }
